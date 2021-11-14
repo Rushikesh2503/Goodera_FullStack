16ac75ba89c4bcd08e8890e24a47d8366760fd01
Rushikesh2503 <rushikeshbambadkar7@gmail.com>
2021-11-14 13:36:17 +0530

    json created

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ data }) => {
+  console.log("dataCard:", data);
   return <div>Card</div>;
 };
 
--- src/Pages/HomePg.jsx
+++ src/Pages/HomePg.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Card from "../Components/Card/Card";
+import { dataa } from "../Pages/dataa";
+import styles from "./Homepg.module.css";
 const HomePg = () => {
+  // console.log(dataa);
+  const [data, setData] = useState(dataa[0]);
+
   return (
     <div>
       <Navbar />
-      <div>{/* <Card /> */}</div>
+      <div className={styles.mainContHome}>
+        {new Array(10).fill(data).map((el, id) => (
+          <Card data={el} key={id} />
+        ))}
+      </div>
     </div>
   );
 };
