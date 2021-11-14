80f78d6a4f1e03ed3224d7f88466c203a6edd482
Rushikesh2503 <rushikeshbambadkar7@gmail.com>
2021-11-14 14:30:54 +0530

    json adjusted

--- src/Pages/HomePg.jsx
+++ src/Pages/HomePg.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Card from "../Components/Card/Card";
 import { dataa } from "../Pages/dataa";
 import styles from "./Homepg.module.css";
+import axios from "axios";
 const HomePg = () => {
   // console.log(dataa);
-  const [data, setData] = useState(dataa[0]);
+  const [data, setData] = useState(dataa);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/data")
+      .then((res) => {
+        console.log("res:", res);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log("err:", err);
+      });
+  }, []);
 
   return (
     <div>
       <Navbar />
       <div className={styles.mainContHome}>
-        {new Array(10).fill(data).map((el, id) => (
+        {data.map((el, id) => (
           <Card data={el} key={id} />
         ))}
       </div>
